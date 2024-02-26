928caa4a578bc88204e5396eb14b7b3bae4a32d4
VishwaSid2 <vishwasid2@gmail.com>
2024-02-26 23:07:01 +0530

    26-02-2024

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,6 +1,4 @@
 "use strict";
-console.log("connecting");
-
 // LOADER REFRESH EFFECT DIV
 
 // const loader = document.createElement("div");
@@ -14,19 +12,21 @@ console.log("connecting");
 // });
 
 // NAVBAR TOGGLER
-
 const navbarX = document.querySelector(".nav__menubars");
 const fullnavbar = document.querySelector("header .fullnav");
-const title = document.querySelector(".nav__logotitle span");
+const title = document.querySelector(".nav__logotitle");
 navbarX.addEventListener("click", togglerHandler);
 title.addEventListener("mouseenter", () => {
-  title.classList.toggle("crt");
-  title.textContent = "K.VISHWA";
+  title.classList.add("crt");
+  // title.textContent = "K.VISHWA";
 });
 title.addEventListener("mouseout", () => {
-  title.classList.toggle("crt");
-  title.textContent = "VISHWA.K";
+  setTimeout(() => {
+    title.classList.toggle("crt");
+  }, 2000);
+  // title.textContent = "VISHWA.K";
 });
+
 // ACTIVE CLASS TOGGLER (PARENT ELEMENT)
 function togglerHandler(e) {
   navbarX.classList.toggle("active");
@@ -34,4 +34,11 @@ function togglerHandler(e) {
   document.body.classList.toggle("lw");
   barhide();
 }
-function barhide() {}
+
+// SLIDER CLONE CHILD SCRIPT
+const abms1 = document.querySelector(".abmslider");
+const abms2 = document.querySelector(".abmslider-2");
+const clone1 = abms1.querySelector(".slider-1").cloneNode(true);
+const clone2 = abms2.querySelector(".slider-2").cloneNode(true);
+abms1.appendChild(clone1);
+abms2.appendChild(clone2);
