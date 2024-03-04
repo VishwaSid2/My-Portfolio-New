303b63474f14933612d009efca6eaa05d744a656
VishwaSid2 <vishwasid2@gmail.com>
2024-03-04 17:06:26 +0530

    final commit

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -8,7 +8,7 @@
 //   document.body.appendChild(loader);
 //   setTimeout(() => {
 //     loader.classList.add("loaded");
-//   }, 100);
+//   }, 10);
 // });
 
 // NAVBAR TOGGLER
@@ -32,8 +32,30 @@ function togglerHandler(e) {
   navbarX.classList.toggle("active");
   fullnavbar.classList.toggle("active");
   document.body.classList.toggle("lw");
-  barhide();
 }
+
+// RECENT PROJECTS
+// const pro_holder = document.querySelector(".recent-projects__holder");
+// pro_holder.addEventListener("wheel", (e) => {
+//    console.log(e.deltaX, e.deltaY);
+//    e.preventDefault();
+//    pro_holder.scrollLeft += 600;
+//    if (e.deltaY >= 0) {
+//      pro_holder.scrollLeft += 800;
+//      e.preventDefault();
+//    } else {
+//      pro_holder.scrollLeft -= 800;
+//      e.preventDefault();
+//    }
+// });
+
+// NAVBAR LINKS CONTROLS HIDDER
+const navlink1 = document.querySelectorAll(".fullnav__top__R1 a");
+const navlink2 = document.querySelectorAll(".fullnav__top__R2 a");
+const alllinks = [...navlink1, ...navlink2];
+alllinks.forEach((a, index) => {
+  a.addEventListener("click", togglerHandler);
+});
 
 // SLIDER CLONE CHILD SCRIPT
 const abms1 = document.querySelector(".abmslider");
